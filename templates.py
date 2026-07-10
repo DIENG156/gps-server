f6f588b6aa0a7490df91f161f77b5ca2f55af005
Saliou DIENG <diengsaliou524@gmail.com>
2026-07-10 23:32:06 +0000

    corriger 5 tuiles carte CARTO Voyager

--- templates.py
+++ templates.py
@@ -1735,12 +1735,22 @@ async function loadDashboard(){
 function initMap(){
   if(map)return;
   map=L.map("map",{zoomControl:true}).setView([14.8500,-15.8833],15);
-  L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",{
+
+  const satellite=L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",{
     attribution:"© Esri, Maxar, Earthstar Geographics",
-    maxZoom:20,
-        maxNativeZoom:17,
+    maxZoom:17,
     minZoom:3
-  }).addTo(map);
+  });
+
+  const streets=L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",{
+    attribution:"© OpenStreetMap contributors © CARTO",
+    maxZoom:20,
+    minZoom:3
+  });
+
+  satellite.addTo(map);
+  L.control.layers({"Satellite":satellite,"Plan (rues)":streets}).addTo(map);
+
   poly=L.polyline([],{color:"#FFD700",weight:4,opacity:0.9}).addTo(map);
 }
 
